f2bdc2c9c94193827cdd3540aa13c95d4923fc74
joehsor1 <109639613+joehsor1@users.noreply.github.com>
2025-03-21 08:33:26 -0600

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,4 +1,13 @@
-tiles = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+tiles = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+wins = 
 
-def winner:
-  
+def victory(board);
+  	for i in range(0, 3, 9):
+    	if board[i] == board[i + 1] == board[i + 2] and board[i] != " ":
+      		return board[i]
+      
+	for i in range(3):
+		if board[i] == board[i + 3] == board[i + 6] and board[i] != " ":
+      		return board[i]}
+if board[0] == board[4] == board[8] or board[2] == board[4] == board[6] and board[4] != " ":
+	return board[4]
